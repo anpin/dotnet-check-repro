26cc7a4125d935f165998dc6d0c18d45f5a9611b
Pavel Anpin <pavel@anpin.fyi>
2023-08-12 09:37:54 +0400

    fixed the bug

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   description = "A very basic flake";
+  inputs.nixpkgs.url = "github:anpin/nixpkgs?ref=dotnet-check";
   outputs = inputs@{ self, nixpkgs, ... }: 
   let      
     system = "x86_64-linux";
@@ -7,8 +8,8 @@
   in {
 
 
-    packages.x86_64-linux.fs = pkgs.callPackage ./console-app.nix { dotnet-sdk = pkgs.dotnet-sdk_7; testProjectFile = "./console-app-tests"; };
-    packages.x86_64-linux.cs = pkgs.callPackage ./console-app.nix { dotnet-sdk = pkgs.dotnet-sdk_7; testProjectFile = "./console-app-tests-cs"; };
+    packages.x86_64-linux.fs = pkgs.callPackage ./console-app.nix { dotnet-sdk = pkgs.dotnet-sdk_7; testProjectFile = "./console-app-tests/console-app-tests.fsproj"; };
+    packages.x86_64-linux.cs = pkgs.callPackage ./console-app.nix { dotnet-sdk = pkgs.dotnet-sdk_7; testProjectFile = "./console-app-tests-cs/console-app-tests-cs.csproj"; };
     packages.x86_64-linux.default = self.packages.x86_64-linux.fs;
 
     devShells.x86_64-linux.default = pkgs.callPackage ./shell.nix { };
